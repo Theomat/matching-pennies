d3a950d149eb6cedbcef0470a3fb5765694cbc53
Theomat <theomatricon@gmail.com>
2021-11-17 17:11:40 +0100

    Updated with changes

--- server/server.py
+++ server/server.py
@@ -8,7 +8,6 @@ from flask import Flask, render_template, request
 # -----------------------------------------------------------
 ## Globals
 # -----------------------------------------------------------
-MAX_ROUNDS: int = 20
 game_no: int = 0
 # Compute first free game number
 while os.path.exists(f"./games/{game_no}.csv"):
@@ -19,7 +18,6 @@ while os.path.exists(f"./games/{game_no}.csv"):
 def parse_args() -> SimpleNamespace:
     parser: argparse.ArgumentParser = argparse.ArgumentParser(description="Flask server to run an online matching penny game server.")
     parser.add_argument("--host", type=str, default="0.0.0.0", help="(default: \"0.0.0.0\") the server host")
-    parser.add_argument("-r", "--rounds", type=int, default=MAX_ROUNDS, help=f"(default: {MAX_ROUNDS}) the number of rounds to be played")
     parser.add_argument("-d", "--debug", action="store_true", help="(default: False) debug mode")
 
     return parser.parse_args()
@@ -31,11 +29,11 @@ app: Flask = Flask(__name__)
 
 @app.route('/')
 def index() -> str:
-    return render_template("index.html", max_rounds=MAX_ROUNDS, games_played=game_no)
+    return render_template("index.html", games_played=game_no)
 
 @app.route('/play')
 def play() -> str:
-    return render_template("play.html", max_rounds=MAX_ROUNDS)
+    return render_template("play.html")
 
 @app.route('/save', methods=["POST"])
 def save_game():
@@ -43,9 +41,14 @@ def save_game():
     try:
         # Parse data
         rounds = []
-        for i in range(MAX_ROUNDS):
+        i = 0
+        success = True
+        while success:
             round = request.form.getlist(f"rounds[{i}][]")
-            rounds.append(round)
+            success = len(round) > 0
+            if success:
+                rounds.append(round)
+            i += 1
         # Write data to disk
         with open(f"./games/{game_no}.csv", "w") as fd:
             csv.writer(fd).writerows(rounds)
@@ -60,5 +63,4 @@ def save_game():
 # -----------------------------------------------------------
 if __name__ == "__main__":
     parameters: SimpleNamespace = parse_args()
-    MAX_ROUNDS = parameters.rounds
     app.run(debug=parameters.debug, host=parameters.host)
--- server/static/matching_pennies.js
+++ server/static/matching_pennies.js
@@ -2,6 +2,16 @@ var rounds = []
 var score = [0, 0]
 var startTime = getMillis()
 
+var timeCircleBar;
+var scoreBar, WIDTH, HEIGHT;
+var dataSent = false
+
+const TARGET_TIME = 10000
+const RATIO_FLUC = .0125
+const TRIES = 5
+const MAX_PARTICLES = 1000
+const HIST_SIZE = 10
+
 
 function produceTable(array, caption, col_names, line_names) {
     var caption = "<caption>" + caption + "</caption>"
@@ -35,9 +45,13 @@ function getMillis(){
     return d.getTime();
 }
 
-function sendData(){
+function sendData(try_redirect = true){
+    if (dataSent)
+        return
+    window.alert("Score: You: " + score[0] + " | CPU: " + score[1] + "\nWin rate: " + Math.round(score[0] / rounds.length * 100) + "%\nPayoff: " + (score[0] -score[1]));
+    dataSent = true
     $.post("save", { rounds: rounds }, function (result) {
-        if(result == "Yes"){
+        if (result == "Yes" && try_redirect){
             $(location).attr("href", window.location.origin);
         }
     });
@@ -55,47 +69,39 @@ function nextRound(){
     }
 
     // Update visual elements
-    let hu_score = document.getElementById("score-you")
-    hu_score.innerHTML = score[0]
-    let cpu_score = document.getElementById("score-cpu")
-    cpu_score.innerHTML = score[1]
 
-
-    
     let history = document.getElementById("history")
     let array = []
+    let startRound = Math.max(0, rounds.length - HIST_SIZE)
     for (var i = 0; i < 2; i++){
         let line = []
-        for (var j = 0; j < rounds.length; j++) {
+        for (var j = 0; j < HIST_SIZE - rounds.length; j++) {
+            line.push("")
+        }
+        for (var j = startRound; j < rounds.length; j++) {
             let color = rounds[j][i] == 1 ? "danger" : "primary"
-            let content = "<a class=\"btn btn-lg fw-bold label label-" + color + "\"> </a>"
+            let content = "<a class=\"btn-" + color + " btn-lg fw-bold\"> </a>"
             line.push(content)
         }
+        
         array.push(line)
     }
     let col_names = []
-    for (var i = 0; i < rounds.length; i++){
+    for (var j = 0; j < HIST_SIZE - rounds.length; j++) {
+        col_names.push("")
+    }
+    for (var i = startRound; i < rounds.length; i++){
         let c = rounds[i][0] != rounds[i][1] ? "W" : "L"
         col_names.push("R" + (i + 1) +": " + c)
     }
-    if(numRound <= MAX_ROUNDS){
-        history.innerHTML = produceTable(array, "", col_names, ["You", "CPU"])
-    }
-
-    let round = document.getElementById("round")
-    if (numRound >= MAX_ROUNDS) {
-        round.innerHTML = "Finished"
-        sendData()
-    } else {
-        round.innerHTML = "Round " + (numRound + 1) + "/" + MAX_ROUNDS
-    }
+    history.innerHTML = produceTable(array, "", col_names, ["You", "CPU"])
+    timeCircleBar.renderProgress(100)
     // Reset startTime
     startTime = getMillis()
 }
 
 function humanChoice(choice){
-    let numRound = rounds.length
-    if (numRound >= MAX_ROUNDS)
+    if(dataSent)
         return
     let time = getMillis() - startTime
     let round = [choice, getCPUPlay(), time]
@@ -103,8 +109,173 @@ function humanChoice(choice){
     nextRound()
 }
 
+function toRad(degrees){
+    return degrees / 180 * Math.PI
+}
+
+function roundRect(ctx, x, y, w, h, bl, br){
+    ctx.beginPath()
+    ctx.moveTo(x + bl, y)
+
+    ctx.lineTo(x + w - br, y)
+    ctx.arc(x + w - br, y + br, br, toRad(90), 0, false)
+
+    ctx.lineTo(x + w, y + h - br)
+    ctx.arc(x + w - br, y + h - br, br, 0, toRad(270), false)
+    ctx.lineTo(x + w - br, y + h)
+
+    ctx.lineTo(x + bl, y + h) 
+    ctx.arc(x + bl, y + h - bl, bl, toRad(270), toRad(180), false)
+
+    ctx.lineTo(x, y + bl)
+    ctx.arc(x + bl, y + bl, bl, toRad(180), toRad(90), false)
+    ctx.closePath()
+}
+
+let lastRender = 0
+let hu_particles = []
+let cpu_particles = []
+function renderScoreBar(){
+    let t = getMillis()
+    let dt = t - lastRender;
+    let w = WIDTH
+    let h = HEIGHT
+
+    let ctx = scoreBar
+
+    let barWidth = Math.round(0.6 * w)
+    let barHeight = Math.round(0.6 * h)
+    let tx = (w - barWidth) / 2
+    let ty = (h - barHeight) / 2
+
+    ctx.clearRect(0, 0, w, h)
+    ctx.save()
+    ctx.translate(tx, ty)
+
+    let ratio = rounds.length == 0 ? .5 : score[0] / rounds.length
+    let b = h * 0.1
+
+    
+    ratio += Math.sin(t / 100) * RATIO_FLUC
+    ratio += Math.sin(t / 327) * RATIO_FLUC / 2
+    ratio += Math.sin(t / 1143) * RATIO_FLUC / 4
+    ratio = Math.min(1, Math.max(0, ratio))
+
+    ctx.save()
+    roundRect(ctx, 0, 0, barWidth, barHeight, b, b)
+    ctx.fill()
+    ctx.clip()
+
+    ctx.fillStyle = '#27ae60';
+    ctx.fillRect(0, 0, barWidth * ratio, barHeight)
+
+    ctx.fillStyle = '#f39c12';
+    ctx.fillRect(barWidth * ratio, 0, barWidth * (1 - ratio), barHeight)
+    ctx.restore()
+
+    // let pradius = b  / 4
+
+    // ctx.fillStyle = '#2ecc71';
+    // hu_particles = doParticles(ctx, hu_particles, ratio, -1, ratio, tx, ty, pradius, barWidth, barHeight, dt)
+    // ctx.fillStyle = '#f1c40f';
+    // cpu_particles = doParticles(ctx, cpu_particles, 1 - ratio, 1, ratio, tx, ty, pradius, barWidth, barHeight, dt)
+    ctx.restore()
+
+    let text_height = Math.round(barHeight / 2)
+    ctx.font = text_height+ "px Arial"
+    let m = ctx.measureText("You")
+    ctx.fillStyle = '#2ecc71';
+    ctx.fillText("You", tx / 2 - m.width / 2, ty + barHeight / 2 + m.actualBoundingBoxAscent / 2)
+    ctx.fillStyle = '#f1c40f';
+    m = ctx.measureText("CPU")
+    ctx.fillText("CPU", w - tx / 2 - m .width / 2, ty + barHeight / 2 + m.actualBoundingBoxAscent / 2)
+
+
+
+    lastRender = getMillis();
+}
+
+function doParticles(ctx, particles, chance, direction, ratio, tx, ty, pradius, barWidth, barHeight, dt){
+    let next_particles = []
+    for (var i = 0; i < particles.length; i++) {
+        let part = particles[i];
+        let x = part[0]
+        let y = part[1]
+        let vx = part[2]
+        let vy = part[3]
+        let ticks = part[4]
+        ctx.beginPath()
+        ctx.arc(x, y, pradius, 0, 50, false)
+        ctx.fill()
+        ticks -= dt
+        if (ticks > 0 && x - pradius >= -tx && y - pradius >= -ty && x + pradius <= barWidth + tx && y + pradius <= barHeight + ty) {
+            x += vx * dt / 1000 * pradius
+            y += vy * dt / 1000 * pradius
+            next_particles.push([x, y, vx, vy, ticks])
+        }
+    }
+    for (var i = 0; i < TRIES && next_particles.length < MAX_PARTICLES; ++i) {
+        if (Math.random() < chance) {
+            next_particles.push([barWidth * ratio + direction * pradius, Math.random() * barHeight,
+            direction * (.1 + Math.random()) * barWidth / 2, (2 * Math.random() - 1) * barHeight / 2,
+            Math.round(Math.random() * 800 + 200)])
+        }
+    }
+    return next_particles
+}
+
+
+function blink(duration){
+    if (dataSent)
+        return
+    let overlay = document.getElementById("overlay")
+    overlay.classList.add("blink")
+    setTimeout(() => {
+        overlay.classList.remove("blink")
+    },
+    duration)
+}
 
 // MAIN
 $(document).ready(function () {
+
+    let canvas = document.getElementById("score")
+    
+    canvas.width = canvas.clientWidth
+    canvas.height = canvas.clientHeight
+    
+    scoreBar = canvas.getContext("2d");
+
+    WIDTH = canvas.width;
+    HEIGHT = canvas.height;
+
+    
+    timeCircleBar = new Circlebar({
+        element: ".time-bar",
+        skin: "fire",
+        type: "manual",
+        maxValue: 100,
+    });
     nextRound()
+
+    let lastBlink = 0
+    setInterval(() => {
+        let x = (getMillis() - startTime) / 1000;
+        let value = 1/20 * Math.log((x *x) / 0.11 + 1) / Math.log(1.7);
+        let progress = Math.round(100 * (1 - value));
+        timeCircleBar.renderProgress(Math.max(0, progress));
+        if (progress < 50){
+            let nextDuration = Math.max(100, (1 - progress / 50) * 1000)
+            if(getMillis() - lastBlink > 1000 - nextDuration){
+                lastBlink = getMillis() + nextDuration
+                blink(nextDuration)
+            }
+        }
+    }, 100)
+
+    setInterval(renderScoreBar, 20)
+});
+
+$(window).bind('beforeunload', function () {
+    sendData(false);
 });
